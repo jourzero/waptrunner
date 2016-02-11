e829c1cbd1851b2713a350e7c7d15f1572294b79
T903335 <T903335@L085699.corp.ads>
2016-02-10 23:54:37 -0500

    Minor update

--- README.md
+++ README.md
@@ -10,11 +10,11 @@ Current Feature Set:
 * Dynamic, color-coded issue list, per project
 * Auto-type test names and CWEs 
 * Ability to add new tests based on new CVEs or other
+* CVE search on cvedetails.com
 
 TODO:
-* CVE search from downloaded NVD XML (reimplement)
-* Clean-up and add prep scripts to GIT
+* Add prep scripts to GIT repo
 * Access control (multi-user support)
-* Push sanitized/subset version of my MongoDB to GIT or cloud
+* Push sanitized subset of my MongoDB to GIT or Meteor cloud
 * Export to CSV from UI
 * Maybe export as SSVL for Threadfix import
--- testrunner.js
+++ testrunner.js
@@ -1,14 +1,10 @@
 // Globals
 tPhase       = ""; gTest = {}; gCWE = {}; gEventData = {}; gTmpObj = {};
 gDefaultScope = ""; 
-//gCveRptBase   = "http://localhost:9080/cverpt.php?software=";
-//gCveRptBase   = "https://web.nvd.nist.gov/view/cpe/search/results?keyword=";
-//gCveRptSuffix = "&status=FINAL&orderBy=CPEURI&namingFormat=2.3";
 gCveRptBase   = "http://www.cvedetails.com/google-search-results.php?q=";
 gCveRptSuffix = "";
 gCweUriBase   = "https://cwe.mitre.org/data/definitions/";
 gCveUriBase   = "http://www.cve.mitre.org/cgi-bin/cvename.cgi?name=";
-//gTestRefBase  = "http://localhost:9080/";""
 gTestRefBase  = "/WAPT/";
 //gPrjSubset    = "2016.*";
 //gPrjSubset    = ".*";
