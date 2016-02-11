2d4ef513ccb3ed173b2ea2d17cfc3300bff2868b
T903335 <T903335@L085699.corp.ads>
2016-02-10 23:31:05 -0500

    Minor update

--- testrunner.js
+++ testrunner.js
@@ -2,8 +2,10 @@
 tPhase       = ""; gTest = {}; gCWE = {}; gEventData = {}; gTmpObj = {};
 gDefaultScope = ""; 
 //gCveRptBase   = "http://localhost:9080/cverpt.php?software=";
-gCveRptBase   = "https://web.nvd.nist.gov/view/cpe/search/results?keyword=";
-gCveRptSuffix = "&status=FINAL&orderBy=CPEURI&namingFormat=2.3";
+//gCveRptBase   = "https://web.nvd.nist.gov/view/cpe/search/results?keyword=";
+//gCveRptSuffix = "&status=FINAL&orderBy=CPEURI&namingFormat=2.3";
+gCveRptBase   = "http://www.cvedetails.com/google-search-results.php?q=";
+gCveRptSuffix = "";
 gCweUriBase   = "https://cwe.mitre.org/data/definitions/";
 gCveUriBase   = "http://www.cve.mitre.org/cgi-bin/cvename.cgi?name=";
 //gTestRefBase  = "http://localhost:9080/";""
