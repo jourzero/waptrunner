dfc2c7defa95582c7605d8a2595927da0b3a13f7
jourzero <epaquet@192.168.10.192>
2016-10-12 09:03:30 -0400

    Minor update

--- client/compatibility/util.js
+++ client/compatibility/util.js
@@ -1,6 +1,6 @@
 function stripHtmlTags(html)
 {
-    console("Stripping out HTML tags from " + html);
+    console.log("Stripping out HTML tags from " + html);
     var tmp = document.createElement("DIV");
     tmp.innerHTML = html;
     return tmp.textContent || tmp.innerText || "";
