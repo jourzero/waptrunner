8e4841fb5288396ae863651a66ca353004b9d35c
jourzero <epaquet@192.168.10.192>
2016-10-12 17:33:31 -0400

    Minor update

--- client/compatibility/util.js
+++ client/compatibility/util.js
@@ -5,3 +5,80 @@ function stripHtmlTags(html)
     tmp.innerHTML = html;
     return tmp.textContent || tmp.innerText || "";
 }
+
+// When the Specific Issue Data changes, parse it if it's formatted as a Burp issue) 
+// and save the results to the DB.
+function parseBurpIssueAndSave(){
+    
+    // If the note is Burp-formatted, parse it
+    var notes = $("#INotes").val();
+    var lines = notes.split('\n');
+    var issue="", evidence="", urls="", newNotes="";
+    var urlSection=false;
+    for (var i in lines){
+        var t = lines[i].split(':');
+
+        // Capture the Issue Name
+        if (lines[i].startsWith('Issue:')){
+            issue = t[1].trim();
+        }
+
+        // Capture the URL list
+        else if (lines[i].startsWith('URL(s):')){
+            urlSection = true;
+        }
+
+        else if (urlSection){
+            var url = lines[i];
+            if ((url !== undefined) && (url.length > 0)){
+                url = url.replace("^ - ", "");
+                urls += url + "\n";
+            }
+            else
+                urlSection = false;
+        }
+
+        // Capture the evidence
+        else if (lines[i].startsWith('Evidence:')){
+            evidence = t[1].trim();
+        }
+
+        // Remove "~" from Burp Clipboarder extension
+        else if (lines[i].equals('~')){
+            newNotes += "\n";
+        }
+
+        // Keep the other lines without modification
+        else{
+            newNotes += lines[i] + "\n";
+        }
+    }
+
+    // Push the captured data to the UI and DB
+    if ((issue !== undefined) && (issue.length > 0)){ 
+        saveIssueDataFromUI("#TIssueName", issue);
+        $("#TIssueName").val(issue);
+    }
+    if ((evidence !== undefined) && (evidence.length > 0)){
+        // Decode the Base64 value
+        evidence = decodeURIComponent(Array.prototype.map.call(atob(evidence), function(c) {
+                return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+            }).join('')).trim();
+        saveIssueDataFromUI("#IEvidence", evidence);
+        $("#IEvidence").val(evidence);
+    }
+    if ((urls !== undefined) && (urls.length > 0)){ 
+        saveIssueDataFromUI("#IURIs", urls);
+        $("#IURIs").val(urls);
+    }
+    if (newNotes.length > 0){ 
+        newNotes = stripHtmlTags(newNotes).trim();
+        saveIssueDataFromUI("#INotes", newNotes);
+        $("#INotes").val(newNotes);
+    }
+
+    // Update titles so that mouse-over information matches the content
+    $("#IURIs").attr("title", $("#IURIs").val());
+    $("#IEvidence").attr("title", $("#IEvidence").val());
+    $("#INotes").attr("title", $("#INotes").val());
+}
--- client/js/events.js
+++ client/js/events.js
@@ -101,76 +101,9 @@ Template.home.events({
         updateScreenshots();
     },
     
-    // When the Specific Issue Data changes, parse it if it's formatted as a Burp issue. 
-    // Then, save it to the Issue collection.
+    // When the notes field changes, try to parse it as an issue that comes from Burp Clipboarder. 
     'change #INotes': function (event) {
-
-        // If the note is Burp-formatted, parse it
-        var notes = $("#INotes").val();
-        var lines = notes.split('\n');
-        var issue="", evidence="", urls="", newNotes="";
-        var urlSection=false;
-        for (var i in lines){
-            var t = lines[i].split(':');
-            
-            // Capture the Issue Name
-            if (lines[i].startsWith('Issue:')){
-                issue = t[1].trim();
-            }
-            
-            // Capture the URL list
-            else if (lines[i].startsWith('URL(s):')){
-                urlSection = true;
-            }
-            
-            else if (urlSection){
-                var url = lines[i];
-                if ((url !== undefined) && (url.length > 0)){
-                    url = url.replace("^ - ", "");
-                    urls += url + "\n";
-                }
-                else
-                    urlSection = false;
-            }
-            
-            // Capture the evidence
-            else if (lines[i].startsWith('Evidence:')){
-                evidence = t[1].trim();
-            }
-            
-            // In the updated notes, prune what we've captured above
-            else{
-                newNotes += lines[i] + "\n";
-            }
-        }
-        
-        // Push the captured data to the UI and DB
-        if ((issue !== undefined) && (issue.length > 0)){ 
-            saveIssueDataFromUI("#TIssueName", issue);
-            $("#TIssueName").val(issue);
-        }
-        if ((evidence !== undefined) && (evidence.length > 0)){
-            // Decode the Base64 value
-            evidence = decodeURIComponent(Array.prototype.map.call(atob(evidence), function(c) {
-                    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-                }).join(''));
-            saveIssueDataFromUI("#IEvidence", evidence);
-            $("#IEvidence").val(evidence);
-        }
-        if ((urls !== undefined) && (urls.length > 0)){ 
-            saveIssueDataFromUI("#IURIs", urls);
-            $("#IURIs").val(urls);
-        }
-        if (newNotes.length > 0){ 
-            newNotes = stripHtmlTags(newNotes);
-            saveIssueDataFromUI("#INotes", newNotes);
-            $("#INotes").val(newNotes);
-        }
-        
-        // Update titles so that mouse-over information matches the content
-        $("#IURIs").attr("title", $("#IURIs").val());
-        $("#IEvidence").attr("title", $("#IEvidence").val());
-        $("#INotes").attr("title", $("#INotes").val());
+        parseBurpIssueAndSave();
     },
     
     // When some fields are clicked, increase the text box size
