8ceccca71bc28e5c10c6e4c2d548ac146531fb91
jourzero <epaquet@192.168.10.192>
2016-10-11 23:44:32 -0400

    Minor update

--- common/routes.js
+++ common/routes.js
@@ -268,8 +268,8 @@ function toHtml(objArray, prjName) {
             if ((obj.INotes !== undefined)&&(obj.INotes !== ""))
                 output += "<tr><th class='thID'>Notes: </th><td class='tdID'>" + htmlEncode(obj.INotes, true, 4) + "</td></tr>\n";
             if ((obj.IEvidence !== undefined)&&(obj.IEvidence !== ""))
-                output += "<tr><th class='thID'>Evidence: </th><td class='tdID'>" + htmlEncode(obj.IEvidence, true, 4) + "</td></tr>\n";
-                //output += "<tr><th class='thID'>Evidence: </th><td class='tdID'>" + obj.IEvidence + "</td></tr>\n";
+                //output += "<tr><th class='thID'>Evidence: </th><td class='tdID'>" + htmlEncode(obj.IEvidence, true, 4) + "</td></tr>\n";
+                output += "<tr><th class='thID'>Evidence: </th><td class='tdID'><pre>" + obj.IEvidence + "</pre></td></tr>\n";
                 output += "<tr><th class='thID'>Screenshot(s): </th><td class='tdID'>" + obj.IScreenshots + "</td></tr>\n";
             output += "<tr><td class='Skip'>&nbsp;</td><td class='Skip'>&nbsp;</td></tr>\n";
 	}
