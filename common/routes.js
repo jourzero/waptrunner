ca73263038ce68f132874cce6302c97a18142751
jourzero <epaquet@192.168.10.192>
2016-10-20 13:32:28 -0400

    Minor update

--- common/routes.js
+++ common/routes.js
@@ -201,7 +201,7 @@ function toHtml(objArray, prjName) {
         var output = "<html><head>\n";
         var priority = "N/A", prevPrio = "", prio = -1;
         var cweUriBase   = "https://cwe.mitre.org/data/definitions/";
-        output += "<style>\nbody{width:1200px;}\na{text-decoration:none;}\n.tdID{width:1100px;max-width:1100px;vertical-align:top;word-wrap:break-word;}\n.thID{text-align:right;vertical-align:top;width:80px;}\nth{vertical-align:top;}\nol{padding-left:25px;}\n.HighP{background-color:red;}\n.MediumP{background-color:orange;}\n.LowP{background-color:cyan;}\n.Skip{background-color:#FFFFFF;}\ntr:nth-child(even){background:#EAEAEA;}\ntr:nth-child(odd){background:#F0F0F0;}\n</style>\n";
+        output += "<style>\nbody{width:1200px;}\na{text-decoration:none;}\n.tdID{width:1100px;max-width:1100px;vertical-align:top;word-wrap:break-word;}\n.thID{text-align:right;vertical-align:top;width:80px;}\nth{vertical-align:top;}\nol{padding-left:25px;}\n.HighP{background-color:red;}\n.MediumP{background-color:orange;}\n.LowP{background-color:cyan;}\n.ExcludeP{background-color:#777;}\n.FixedP{background-color:#777;}\n.Skip{background-color:#FFFFFF;}\ntr:nth-child(even){background:#EAEAEA;}\ntr:nth-child(odd){background:#F0F0F0;}\n</style>\n";
         
         // Traverse the array of issue objects
         output += "</head>\n<body>\n";
