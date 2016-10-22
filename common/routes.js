b4c7cd3d85d021e4bc78b6b49df774468494b16c
jourzero <epaquet@192.168.10.192>
2016-10-22 12:40:58 -0400

    Minor update

--- common/routes.js
+++ common/routes.js
@@ -356,8 +356,8 @@ var htmlEncode = function (source, display, tabs, linkify) {
         
         toLink == function(inputText) {
             var replacedText, replacePattern;
-            replacePattern = /^(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])$/gim;
-            replacedText = inputText.replace(replacePattern, '<a href="$1" target="refWin">$1</a>');
+            replacePattern = /^- (\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+            replacedText = inputText.replace(replacePattern, '- <a href="$1" target="refWin">$1</a>');
             return replacedText;
         }
         
